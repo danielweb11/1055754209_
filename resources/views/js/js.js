a95690f9b1dd7c430a6ddb7f1be41bbb87d62508
danielweb11 <daguzmancu@unal.edu.co>
2026-04-27 15:04:17 -0500

    js actualizado

--- resources/views/js/js.js
+++ resources/views/js/js.js
@@ -1,5 +1,18 @@
 function api_parcial() {
     var enPoint="https://fakestoreapi.com/products"
     fetch(endPoint)
-    .then(function(response))
+    .then(function(response)function api_parcial() {
+    var enPoint="https://fakestoreapi.com/products"
+    fetch(endPoint)
+    .then(function(response){
+        return response.json()
+        
+    })
+    .then(function(data){
+        var valores_x=[]
+        for (let i = 0; i < valores_x.length; i++) {
+            valores_x.push(data[i].rating)
+        }
+    })
+}
 }
